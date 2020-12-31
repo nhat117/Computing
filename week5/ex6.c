6f256e4eae49c7f0df79428742e327b5d39eefbb
Nhat Bui <s3878174@rmit.edu.vn>
2020-12-31 00:14:55 +0000

    change in week 5

--- week5/ex6.c
+++ week5/ex6.c
@@ -1,25 +1,17 @@
 #include <stdio.h>
-#include <ctype.h>
 
-int main(void) {
-	char arr [5][100] = {};
-	for (int i = 0; i < 5; i ++ ) {
-		printf ("Please enter full name %d", ++i);
-		scanf("%[^\n]s", arr[i]);
-		}
-	
-
-	//Determine the first name
-	for (int i = 0; i < 5; i++) {
-		for(int j = 0; j < 100; j++) {
-			if (isspace(arr[i][j])) {
-				for (int k = 0; k < j; k++) {
-					printf("%c", arr[i][k]);
-				}
-			}
-		} 
-		printf(" ");
-	}
-
-	return 0;
-} 
+int main(void) 
+{
+    char fullnam[5][20];
+    for (int i = 0; i < 5; i++)
+    {
+        printf("Enter fullname %d: ", i + 1);
+        scanf("%[^\n]s", fullnam[i]);
+        getchar();
+    }
+  
+  for (int i = 0; i < 5; i++){
+      printf("-%s-", fullnam[i]);
+  }
+  return 0;
+}
